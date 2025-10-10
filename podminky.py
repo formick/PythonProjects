acc7f2f417d8b87bcb838af8cf0bf76302019ccc
D32\zak <spseplzen@spseplzen.cz>
2025-10-10 12:25:33 +0200

    podminky 10.10

---
+++ podminky.py
@@ -0,0 +1,30 @@
+vstup = input("zadej cislo: ")
+try:
+    cislo = int(vstup)
+except:
+    cislo = 0
+    print("vstup = " + vstup)
+
+if cislo > 5:
+        print("cislo")
+        print(cislo)
+        print("vetsi")
+        print("je")
+        print("vetsi nez 5")
+        if cislo > 10:
+            print("nevim, neco")
+
+else:
+    print("neni vetsi nez 5")
+if cislo < 7:
+    print("cislo je mensi nez 7")
+print("konec")
+
+a = input("zadej A")
+b = input("zadej B")
+c = input("zadej C")
+
+
+print("nula reseni v R.")
+print("jedno reseni v R.")
+print("dve reseni v R.")
